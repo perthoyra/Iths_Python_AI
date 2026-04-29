ec198408eb81dd40493bb92004ec06f2b3e03ac1
PerThoyra <per.thoyra@outlook.com>
2026-04-29 09:02:57 +0200

    Simple menu

--- week2/main.py
+++ week2/main.py
@@ -1,8 +1,42 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+from os import system, name
+from time import sleep
+import menu
 
 
+def clear_screen() -> None:
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+def main():
+    thisdict = {
+        1: "Select option 1.",
+        2: "Select option 2.",
+        0: "Exit"
+        }
+    
+    myMenu = menu.MyMenu(thisdict)
+    
+    while True:
+        clear_screen()
+        myMenu.draw_menu()
+        selected = myMenu.get_input()
+        
+        print(selected)
+        
+        match selected:
+            case 0:
+                print("Terminating program...")
+                break
+            case 1,2:
+                print("\n")
+                print(thisdict[selected])
+                print("\n")
+                sleep(2)
+
+        # it is a bit quick, so the resulkt is not visible long enough
+        _ = input()
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi("Someone")
+    main()
